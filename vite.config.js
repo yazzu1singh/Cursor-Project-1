73c907717d611b7cff22111ed2026951b8829afc
yazzu1singh <yazzu1.sin@example.com>
2026-02-12 00:44:53 +0200

    Deploying fixed assets to root

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // This tells the browser: "Look inside the /Cursor-Project-1/ folder for my CSS/JS"
-  base: '/Cursor-Project-1/', 
   plugins: [react()],
+  // This tells the browser: "Look for assets relative to where index.html is"
+  base: './', 
 })
